fe70ed1453200231dbe8072a9944dc19371c7e03
ndbookout <ndbookout@hotmail.com>
2015-12-14 22:51:59 -0700

    REDO SHIT!

---
+++ Eolin & the Golden Tree/Assets/Scripts/Environment/MoveGlobally.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using MinMax;
+
+public class MoveGlobally : MonoBehaviour 
+{
+	public enum Axis
+	{
+		x,
+		y,
+		both
+	}
+
+	public Axis axis;
+	public float speed;
+	public Range range;
+
+	private Transform objTrans;
+	private bool moveDown = true;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (axis == Axis.x)
+        {
+			if (objTrans.position.x >= range.max)
+            {
+				Switch ();
+			} else if (objTrans.position.x <= range.min)
+            {
+				Switch ();
+			}
+		}
+        else if (axis == Axis.y)
+        {
+			if (objTrans.position.y >= range.max)
+            {
+				Switch ();
+			} else if (objTrans.position.y <= range.min)
+            {
+				Switch ();
+			}
+		} else if (axis == Axis.both)
+        {
+			Debug.Log ("Fuck you, you greedy bastard.");
+		}
+
+		Move ();
+	}
+
+	void Move()
+	{
+        //include Axis.x later
+        if (moveDown) 
+		{
+			objTrans.position -= new Vector3 (0, -1, 0) * speed * Time.deltaTime;
+		} 
+		else 
+		{
+			objTrans.position -= new Vector3 (0, 1, 0) * speed * Time.deltaTime;
+		}
+	}
+
+	void Switch()
+	{
+		moveDown = !moveDown;
+	}
+}
---
+++ Eolin & the Golden Tree/Assets/Scripts/MinMaxStruct.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MinMax
+{
+
+    public class InvalidRangeException : System.Exception
+    {
+        public InvalidRangeException(float min, float max)
+        {
+            Debug.LogError("Invalid Range Specified. " + max + " is less than " + min + ".");
+        }
+    } 
+
+    public struct Range
+    {
+        public float min, max;
+        public float range
+        {
+            get { return max - min; }
+        }
+
+        public Range(float min, float max)
+        {
+            this.min = min;
+            this.max = max;
+
+            if (this.max < this.min)
+            {
+                throw new InvalidRangeException(min, max);
+            }
+        }
+
+        public static implicit operator Range(Vector2 vector)
+        {
+            Range vectorRange = new Range(vector.x, vector.y);
+            return vectorRange;
+        }
+        public static implicit operator Vector2(Range range)
+        {
+            Vector2 rangeVector = new Vector2(range.min, range.max);
+            return rangeVector;
+        }
+        
+        public static bool operator < (Range a, Range b)
+        {
+            if (a.range < b.range)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator > (Range a, Range b)
+        {
+            if (a.range > b.range)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator <= (Range a, Range b)
+        {
+            if (a.range <= b.range)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator >= (Range a, Range b)
+        {
+            if (a.range >= b.range)
+                return true;
+            else
+                return false;
+        }
+
+        public override string ToString()
+        {
+            return "Range(" + min.ToString("N1") + ", " + max.ToString("N1") + ")";
+        }
+    }
+}
